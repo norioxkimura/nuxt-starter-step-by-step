a8b77a3bc147d19c5d3a70d7691521020a4f722d
Norio Kimura <kimura@monad.tv>
2021-03-15 21:34:51 +0900

    : template (nuxt)

---
+++ nuxt.config.js
@@ -0,0 +1,23 @@
+export default {
+  srcDir: 'src',
+  ssr: false,
+  target: 'static',
+  head: {
+    title: '{ title }',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+  css: [],
+  plugins: [],
+  components: true,
+  buildModules: ['@nuxtjs/composition-api'],
+  modules: [],
+  build: {},
+}
