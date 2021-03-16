2dff3ea872aab963896d1cdaabedcca3a15dee2a
Norio Kimura <kimura@monad.tv>
2021-03-16 09:45:19 +0900

    : (example): portal

---
+++ src/components/Notification.vue
@@ -0,0 +1,31 @@
+<template>
+  <portal to="notification">
+    <div v-if="open">
+      <div>{{ message }}</div>
+      <button @click="clicked()">OK</button>
+    </div>
+  </portal>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+export default defineComponent({
+  props: {
+    message: {
+      type: String,
+      default: 'Unexpected error.',
+    },
+    open: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup(_, { emit }) {
+    return {
+      clicked: () => {
+        emit('close')
+      },
+    }
+  },
+})
+</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,23 @@
 <template>
-  <div>hello world</div>
+  <div>
+    <portal-target name="notification" />
+    <div>hello world</div>
+    <notification :open="openNotification" @close="openNotification = false" />
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+export default defineComponent({
+  setup() {
+    const openNotification = ref(false)
+    ;(async () => {
+      await new Promise((resolve) => setTimeout(resolve, 1000))
+      openNotification.value = true
+    })()
+    return {
+      openNotification,
+    }
+  },
+})
+</script>
