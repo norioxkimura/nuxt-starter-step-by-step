7844a990420846b150c08f351ba70f5610cc500e
Norio Kimura <kimura@monad.tv>
2021-03-17 13:07:32 +0900

    : simple ref & reactive

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,19 @@
 <template>
-  <div>hello world</div>
+  <div>
+    <input v-model="container.text" type="text" />
+    <input v-model="text" type="text" />
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, reactive, toRefs } from '@nuxtjs/composition-api'
+export default defineComponent({
+  setup() {
+    const container = reactive({ text: 'hello' })
+    return {
+      container,
+      ...toRefs(container),
+    }
+  },
+})
+</script>
